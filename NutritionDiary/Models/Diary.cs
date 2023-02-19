b57d2686d80dd00c69d2c3ea7df5a3ce0315a0e2
Mykhailo <misha.vrana@gmail.com>
2023-02-19 16:10:05 +0200

    Initial commit

---
+++ NutritionDiary/Models/Diary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionDiary.Models
+{
+    public class Diary
+    {
+        private List<Week> _weeks;
+
+        public Diary() 
+        { 
+            _weeks= new List<Week>();
+        }
+
+        // Adding a weeek
+        public void AddNote(DateTime startDate, DateTime endDate, string product)
+        {
+            Week week = new Week(startDate, endDate, product);
+            _weeks.Add(week);
+        }
+
+        // Deleting a week
+        public void DeleteNote (DateTime id)
+        {
+            foreach (Week note in _weeks)
+            {
+                _weeks.Where(note => note.Id == id.ToString());
+            }
+        }
+    }
+}
---
+++ NutritionDiary/Models/Week.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionDiary.Models
+{
+    public class Week
+    {
+        public string Id {  get; set; }    
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; }
+        public IDictionary<DateTime, Reaction> DaysAndReactions { get; set; }
+        public string Product { get; set; }
+
+        // If there is at least one bad reaction on the product it returns "Bad", otherwise the reaction is "Good"
+        // It sets the reaction to the current date in the "DaysAndReactions" list
+        public string Reaction
+        {
+            get 
+            {
+                foreach(Reaction reaction in DaysAndReactions.Values)
+                {
+                    if(reaction == Models.Reaction.Bad)
+                        return ("Bad");
+                    else
+                        return ("Good");
+                }
+                return "Undefined";
+            }
+            set
+            {
+                Reaction reaction = Models.Reaction.Undifined;
+
+                if(value == "Good")
+                    reaction = Models.Reaction.Good;
+                else 
+                    reaction = Models.Reaction.Bad; 
+
+                DaysAndReactions[DateTime.Now] = reaction;
+            }
+        }
+
+        public Week(DateTime startDate, DateTime endDate, string product)
+        {
+            Id = startDate.ToString();
+            StartDate = startDate;
+            EndDate = endDate;
+            DaysAndReactions = new Dictionary<DateTime, Reaction>();
+            Product = product;
+        }
+    }
+}
